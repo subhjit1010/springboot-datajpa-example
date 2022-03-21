ff62f312295374649e96733e7755689b18bd509a
w90941 <subhjit.dutta@sensis.com.au>
2022-03-21 16:34:34 +1100

    test file added

--- src/test/java/com/example/springbootdatajpa/SpringBootDataJpaApplicationTests.java
+++ src/test/java/com/example/springbootdatajpa/SpringBootDataJpaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springbootdatajpa;
+/*package com.example.springbootdatajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class SpringBootDataJpaApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
